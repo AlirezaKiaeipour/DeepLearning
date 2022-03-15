a1d470a422e1dd3c8bbefdecfe48c9dc37374bb0
Acer Acer <a.kiaipoor@gmail.com>
2022-03-15 15:20:52 +0330

    Waste Classification

---
+++ Waste-Classification/inference.py
@@ -0,0 +1,21 @@
+import argparse
+import numpy as np
+import cv2
+from tensorflow.keras.models import load_model
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_model",type=str,help="Please Enter path of input model/  Example: model.h5")
+parser.add_argument("--input_image",type=str,help="Please Enter path of input image/  Example: image.jpg")
+arg = parser.parse_args()
+
+model = load_model(arg.input_model)
+
+image = cv2.imread(arg.input_image)
+image = cv2.resize(image,(299,299))
+image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+image = image / 255.0
+image = image.reshape(1,299,299,3)
+
+predict = np.argmax(model.predict(image))
+predict = np.where(predict==0,"Organic","Recyclable")
+print(predict)
